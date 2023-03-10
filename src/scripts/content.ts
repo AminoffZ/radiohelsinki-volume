4b2b4163a29c2385630e1d673d12a8e61cfa6d9e
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2023-03-10 05:43:49 +0200

    Better slider positioning

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,18 +1,11 @@
-var styleRef = document.createElement("link");
-styleRef.rel = "stylesheet";
-styleRef.href = chrome.runtime.getURL("styles/audio-controller.css");
-console.log(styleRef.href);
-console.log(styleRef);
-(document.head || document.documentElement).appendChild(styleRef);
-
 const audioController = document.createElement("nav");
 audioController.className = "audio-controller prevent-select";
 
 audioController.innerHTML = `
-<span class="material-symbols-outlined audio-symbol" style="font-size: 18px">
-  volume_up
-</span>
 <span class="audio-slider-container">
+  <span class="material-symbols-outlined audio-symbol" style="font-size: 18px">
+    volume_up
+  </span>
   <input class="styled-slider slider-progress" type="range" id="volume" name="volume" min="0" max="100" step="1" />
 </span>
 `;
@@ -23,7 +16,7 @@ googleIcons.href =
   "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0";
 
 (document.head || document.documentElement).appendChild(googleIcons);
-(document.body || document.documentElement).appendChild(audioController);
+document.querySelector(".player-info-wrapper").appendChild(audioController);
 
 const muteButtonScript: HTMLScriptElement = document.createElement("script");
 muteButtonScript.src = chrome.runtime.getURL("mute-button.js");
